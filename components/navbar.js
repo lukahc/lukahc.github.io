ca02087cd781dfd2a06f13be7a85bd5f3cc3d7fa
Luka <luka.cowley@softwire.com>
2023-05-10 11:22:24 +0100

    Added Navbar component

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <nav class="navbar">
+                <p>Home</p>
+                <p>Page</p>
+                <p>Page</p>
+                <p>Page</p>
+            </nav>
+        `;
+    }
+}
+
+customElements.define("Navbar", Navbar);
