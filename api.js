98e5875c94bea02fc3e2d338cb457937b7c05b99
Luka <luka.cowley@softwire.com>
2023-05-17 12:18:24 +0100

    Display capitalised name

--- api.js
+++ api.js
@@ -1,5 +1,17 @@
 async function getName(num) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
-    const json = await res.json();
-    console.log(json.name);
+    const resultE = document.querySelector("#result");
+    const submitE = document.querySelector("#submit");
+    submitE.value = "Loading...";
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
+        const json = await res.json();
+        resultE.innerHTML = toTitleCase(json.name);
+    } catch {
+        resultE.innerHTML = "Invalid input";
+    }
+    submitE.value = "Submit";
+    resultE.className = "result";
+}
+function toTitleCase(string) {
+    return string[0].toUpperCase() + string.slice(1);
 }
