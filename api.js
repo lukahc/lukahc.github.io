faa12d35b467034cbe4864e521e2aee010109064
Luka <luka.cowley@softwire.com>
2023-05-24 11:15:19 +0100

    Added error element

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 async function getName(num) {
     const resultE = document.querySelector("#result");
+    const errorE = document.querySelector("#error");
     const nameE = document.querySelector("#name");
     const typesE = document.querySelector("#types");
     const spriteE = document.querySelector("#sprite");
@@ -8,12 +9,14 @@ async function getName(num) {
     try {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
         const json = await res.json();
-        resultE.innerHTML = toTitleCase(json.name);
+        nameE.innerHTML = toTitleCase(json.name);
+        errorE.className = "error hidden";
+        resultE.className = "result";
     } catch {
-        resultE.innerHTML = "Invalid input";
+        errorE.className = "error";
+        resultE.className = "result hidden";
     }
     submitE.value = "Submit";
-    resultE.className = "result";
 }
 function toTitleCase(string) {
     return string[0].toUpperCase() + string.slice(1);
